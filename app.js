247aa16c24793cc31d83883f99f4c8e966c7aad2
Pablo Fiorido <pablofiorido@hotmail.com>
2020-07-21 19:14:28 -0300

    Se agrega al formulario para calcular ademas de MM1, el sistema MD1

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ var listLq = null
 var listWq = null
 var listP0 = null
 
+var lambda2 = null
+var mu2 = null
+var Ro2 = null
+var Ls2 = null
+var Ws2 = null
+var Lq2 = null
+var Wq2 = null
+var Pcero2 = null
+var sendBtn2 = null
+var listRo2 = null
+var listLs2 = null
+var listWs2 = null
+var listLq2 = null
+var listWq2 = null
+var listP02 = null
 
 var sendForm = function (evt) {
 
@@ -51,6 +66,43 @@ var sendForm = function (evt) {
 
 
 
+
+var sendForm2 = function (evt) {
+
+    evt.preventDefault()
+    if (lambda2.value > 0 && mu2.value > 0) {
+        Ro2 = parseFloat(lambda2.value) / parseFloat(mu2.value)
+        if (Ro2 < 1) {
+            Lq2 = (Ro2 * Ro2) / (2 * (1 - Ro2))
+            Wq2 = Lq2 / (parseFloat(lambda2.value))
+            Ws2 = Wq2 + (1 / (parseFloat(mu2.value)))
+            Ls2 = (parseFloat(lambda2.value)) * Ws2
+            Pcero2 = 1 - Ro2
+
+            listRo2.innerHTML = Ro2
+            listLs2.innerHTML = Ls2
+            listWs2.innerHTML = Ws2
+            listLq2.innerHTML = Lq2
+            listWq2.innerHTML = Wq2
+            listP02.innerHTML = Pcero2
+
+            console.log(Ro2)
+            console.log(Ls2)
+            console.log(Ws2)
+            console.log(Lq2)
+            console.log(Wq2)
+            console.log(Pcero2)
+        } else {
+            listRo2.innerHTML = Ro2
+            window.alert('SERVIDOR SATURADO. No se pueden calcular medidas de desempeño')
+        }
+
+    }
+
+}
+
+
+
 window.onload = function () {
 
     lambda = document.getElementById('lambda')
@@ -65,4 +117,17 @@ window.onload = function () {
 
     sendBtn = document.getElementById('send')
     sendBtn.onclick = sendForm
+
+    lambda2 = document.getElementById('lambda2')
+    mu2 = document.getElementById('mu2')
+
+    listRo2 = document.getElementById('listRo2')
+    listLs2 = document.getElementById('listLs2')
+    listWs2 = document.getElementById('listWs2')
+    listLq2 = document.getElementById('listLq2')
+    listWq2 = document.getElementById('listWq2')
+    listP02 = document.getElementById('listP02')
+
+    sendBtn2 = document.getElementById('send2')
+    sendBtn2.onclick = sendForm2
 }
